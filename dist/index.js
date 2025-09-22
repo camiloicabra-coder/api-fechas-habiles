f7a62c14ac8b52e3fde8da3ac8a5d5cbb669e2a9
Camilo Cabra <camiloi.cabra@gmail.com>
2025-09-21 19:01:55 -0500

    fix: limpiar dist y recompilar para vercel

--- dist/index.js
+++ dist/index.js
@@ -7,24 +7,16 @@ const express_1 = __importDefault(require("express"));
 const workingDate_routes_1 = __importDefault(require("./routes/workingDate.routes"));
 const calendar_utils_1 = require("./utils/calendar.utils");
 const app = (0, express_1.default)();
-// Middleware
 app.use(express_1.default.json());
-// Ruta raíz
-app.get("/", (req, res) => {
-    res.json({ message: "API de Fechas Hábiles funcionando 🚀" });
-});
-// Rutas de negocio
-app.use("/working-date", workingDate_routes_1.default);
+app.use("/", workingDate_routes_1.default);
 // Cargar festivos
 (async () => {
     await (0, calendar_utils_1.loadHolidays)();
 })();
-// Si estamos en local, arrancamos con app.listen
 if (process.env.NODE_ENV !== "production") {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-        console.log(`✅ Server running on http://localhost:${PORT}`);
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
 }
-// 👉 En Vercel no usamos app.listen, solo exportamos
 exports.default = app;
--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,20 @@ import { loadHolidays } from "./utils/calendar.utils";
 
 const app = express();
 
-// Middleware
 app.use(express.json());
-
-// Ruta raíz
-app.get("/", (req, res) => {
-  res.json({ message: "API de Fechas Hábiles funcionando 🚀" });
-});
-
-// Rutas de negocio
-app.use("/working-date", workingDateRoutes);
+app.use("/", workingDateRoutes);
 
 // Cargar festivos
 (async () => {
   await loadHolidays();
 })();
 
-// Si estamos en local, arrancamos con app.listen
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
   });
 }
 
-// 👉 En Vercel no usamos app.listen, solo exportamos
 export default app;
+
